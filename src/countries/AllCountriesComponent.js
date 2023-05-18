d27e706a74f940d5f05fd2a1e766c35e78d940bc
Isabel <isabelgalwey@gmail.com>
2023-05-18 17:57:16 +0100

    made initial list

--- src/countries/AllCountriesComponent.js
+++ src/countries/AllCountriesComponent.js
@@ -1,9 +1,20 @@
 
-
-const AllCountriesComponent = () => {
+const AllCountriesComponent = ({country}) => {
 return (
-    <p>This is where I could go!</p>
+
+    // <h3>{country.name.common}</h3>
+    // <p>{country.capital[0]}</p>
+    <>
+    {/* <p>This is where I could go!</p> */}
+    <li>{country.name.common} {country.flag}</li>
+    <button type="submit">Add to list</button>
+    </>
+
 );
 }
 
+
+// add a button which adds the country to a new blank array
+// create a function that adds the country to the array
+// map the array into visitedcountries 
 export default AllCountriesComponent;
--- src/countries/CountriesContainer.js
+++ src/countries/CountriesContainer.js
@@ -4,16 +4,21 @@ import VisitedCountriesComponent from "./VisitedCountriesComponent";
 
 const CountriesContainer = () => {
 
-    const [listOfCountries, setListOfCountries] = useState(null);
+    const [listOfCountries, setListOfCountries] = useState([]);
 
     const fetchCountries = async () => {
         const response = await fetch("https://restcountries.com/v3.1/all")
-        const jsonData = await response.json(); 
-        
+        const jsonData = await response.json();    
         setListOfCountries(jsonData);
     }
 
     console.log(listOfCountries);
+
+    const allCountries = listOfCountries.map((country) => {
+            return <AllCountriesComponent country={country}/>
+        })    
+    
+        
 
     useEffect(() => {
         fetchCountries();
@@ -21,9 +26,9 @@ const CountriesContainer = () => {
 
 return (
     <>
-    <p>hello world</p>
-    <AllCountriesComponent/>
+    <h3>Travel Bucket List</h3>
     <VisitedCountriesComponent/>
+    {allCountries}
     </>
 );
 }
